03b719e5bcfe5f5be6d6fecef5d12b121813e0fe
kmasiak <kmasiak2@gmail.com>
2022-10-11 22:19:49 -0400

    Added folding

--- cells.c
+++ cells.c
@@ -11,7 +11,8 @@ float maxdist = 0;
 
 // these are global for now but i guess they don't need to be
 int xcoords[NUM_POINTS], ycoords[NUM_POINTS];
-float distances[IMAGE_SIZE][IMAGE_SIZE], colors[IMAGE_SIZE][IMAGE_SIZE];
+float distances[IMAGE_SIZE][IMAGE_SIZE];
+float folded[IMAGE_SIZE][IMAGE_SIZE];
 float colors[IMAGE_SIZE][IMAGE_SIZE];
 
 // generate some random data as a starting point
@@ -27,7 +28,7 @@ void generate_data() {
 // points (NUM_POINTS) and calculate the distance to the closest one
 // this is pretty inefficient as it stands but that might lend itself
 // well to the parallelization that'll take place later on
-float distance() {
+void distance() {
   for (int x = 0; x < IMAGE_SIZE; x++) {
     for (int y = 0; y < IMAGE_SIZE; y++) {
       mindist = INFINITY;
@@ -43,19 +44,54 @@ float distance() {
   }
 }
 
+//The folding technique is used here
+//Fold each value 3 times
+//There are two triple nested for loops that will
+//be parallelized later on for improved performance
+void folding() {
+  float scale_A[IMAGE_SIZE][IMAGE_SIZE];
+  float fold_A[IMAGE_SIZE][IMAGE_SIZE]; 
+
+  for (int nfolds = 0; nfolds < 3; nfolds++)  {
+    float min_A = distances[0][0];
+    float max_A = distances[0][0];
+    for (int x = 0; x < IMAGE_SIZE; x++) {
+        for (int y = 0; y < IMAGE_SIZE; y++) {
+            if (min_A > distances[x][y]) {
+                min_A = distances[x][y];
+            } else if (max_A < distances[x][y]) {
+                max_A = distances[x][y];
+            }
+        } 
+    }  
+    max_A = max_A - min_A;
+
+    for (int x = 0; x < IMAGE_SIZE; x++) {
+        for (int y = 0; y < IMAGE_SIZE; y++) {
+            if (nfolds == 0) {
+                folded[x][y] = distances[x][y];
+            }
+            scale_A[x][y] = folded[x][y] - min_A;
+            fold_A[x][y] = max_A / (nfolds + 1);
+            folded[x][y] = fabs(scale_A[x][y] - fold_A[x][y]);
+        } 
+    } 
+  }
+}
+
 // basically we just want to convert each value in distances
 // to a meaningful color value. as points get closer together,
 // they get darker
 void color_convert() {
   for (int x = 0; x < IMAGE_SIZE; x++) {
     for (int y = 0; y < IMAGE_SIZE; y++) {
-      if (distances[x][y] < mindist) {
-        mindist = distances[x][y];
-      } else if (distances[x][y] > maxdist) {
-        maxdist = distances[x][y];
+      if (folded[x][y] < mindist) {
+        mindist = folded[x][y];
+      } else if (folded[x][y] > maxdist) {
+        maxdist = folded[x][y];
       }
 
-      colors[x][y] = (distances[x][y] - mindist) / (maxdist - mindist);
+      colors[x][y] = (folded[x][y] - mindist) / (maxdist - mindist);
     }
   }
 }
@@ -65,6 +101,7 @@ int main() {
 
   generate_data();
   distance();
+  folding();
   color_convert();
 
   // write data to a file for graphing purposes
