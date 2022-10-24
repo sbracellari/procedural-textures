2534b0140bf1fa7feeb2096e1dc1a09e212f3f13
kmasiak <kmasiak2@gmail.com>
2022-10-24 18:34:32 -0400

    Measures execution time in ms

--- cells.c
+++ cells.c
@@ -3,13 +3,16 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define true 1
 #define false 0
 
 int main() {
+  long start = clock();
   int do_io = true;
   generate_files(do_io);
-
+  double duration = (double)(clock() - start) / (CLOCKS_PER_SEC / 1000);
+  printf("Time Taken: %f ms\n", duration);
   return 0;
 }
